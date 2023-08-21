5906754b36d3883615e1dfc0fc94f5e5487426c8
krish0674 <118186942+krish0674@users.noreply.github.com>
2023-08-21 20:03:44 +0530

    Add files via upload

---
+++ code/train.py
@@ -0,0 +1,23 @@
+import torch
+
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+model = BERT()
+batch = make_batch()
+input_ids, segment_ids, masked_tokens, masked_pos, isNext = map(torch.LongTensor, zip(*batch))
+
+for epoch in range(100):
+    optimizer.zero_grad()
+    logits_lm, logits_clsf = model(input_ids, segment_ids, masked_pos)       
+    loss_lm = criterion(logits_lm.transpose(1, 2), masked_tokens) # for masked LM
+    loss_lm = (loss_lm.float()).mean()
+    loss_clsf = criterion(logits_clsf, isNext) # for sentence classification
+    loss = loss_lm + loss_clsf
+    if (epoch + 1) % 10 == 0:
+        print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.6f}'.format(loss))
+    loss.backward()
+    optimizer.step()
+
+logits_lm, logits_clsf = model(input_ids, segment_ids, masked_pos)
