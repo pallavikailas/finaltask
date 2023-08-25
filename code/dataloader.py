f9099359ffc9d7cffe25128299218d15b96c8d99
krish0674 <118186942+krish0674@users.noreply.github.com>
2023-08-25 22:42:39 +0530

    Add files via upload

--- code/dataloader.py
+++ code/dataloader.py
@@ -1,63 +1,31 @@
+import pandas as pd
 import torch
-from torch.utils.data import DataLoader
-from torch.utils.data import Dataset
-import pandas as pd 
-import os 
-
-class Vocabulary:
-    def __init__(self):
-        # Initialize token-to-index mapping with special tokens
-        self.stoi = {"<SOS>": 0, "<EOS>": 1, "<UNK>": 2}
-        # Initialize index-to-token mapping
-        self.itos = {0: "<SOS>", 1: "<EOS>", 2: "<UNK>"}
+from torch.utils.data import DataLoader, Dataset
 
 class CustomDataset(Dataset):
+    def __init__(self, data):
+        self.data = data
 
-    def __init__(self, file, transform=None,vocab = None):
-        current_dir = os.getcwd()
-
-        file_path = os.path.join(current_dir, file)
-
-        # Load the CSV file
-        self.df = pd.read_csv("D:/final_task-5/preprocessed_train.csv")
-        self.transform = transform
-
-        self.vocab = vocab
-
-        self.text = self.df["text"]
-        self.y = self.df.drop(["text"], axis=1)
-        
     def __len__(self):
-        return len(self.df)
+        return len(self.data)
 
-    def numericalize(self, text):
-        return [self.vocab.stoi.get(token, self.vocab.stoi["<UNK>"]) for token in text.split()]
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        return item["text"], item["is_humor"]
 
-    def __getitem__(self, index):
-        text = self.text[index]
-        y = self.y.iloc[index] 
+# Load the CSV file
+train = pd.read_csv(r'C:\Users\KRISH DIDWANIA\final_task-3\preprocessed_train.csv')
 
-        if self.transform is not None:
-            text = self.transform(text)
+# Select required columns and convert to a list of dictionaries
+train_data = train[["text", "is_humor"]].to_dict(orient='records')
+# Create an instance of your CustomDataset
+custom_dataset = CustomDataset(train_data)
 
-        numericalized_text = [self.vocab.stoi["<SOS>"]]
-        numericalized_text += self.numericalize(text)
-        numericalized_text.append(self.vocab.stoi["<EOS>"])
+# Define DataLoader parameters
+batch_size = 16
+shuffle = True  # Set to True for training, False for validation/testing
 
-        return torch.tensor(numericalized_text), y
-
-vocab = Vocabulary()
-
-# Create an instance of the CustomDataset
-dataset = CustomDataset(file='final_task-5/preprocessed_train.csv',vocab = vocab )
-
-# Access an item from the dataset
-sample_input, sample_label = dataset[0]
-
-# Print the tensor
-print("Sample Input Tensor:")
-print(sample_input)
-
-print("\nSample Label:")
-print(sample_label)
-
+# Create a DataLoader instance
+dataloader = DataLoader(custom_dataset, batch_size=batch_size, shuffle=shuffle)
+    
+           
