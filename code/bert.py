9ccce3dfc711c6202d3a41e293a54fc5b7600144
krish0674 <118186942+krish0674@users.noreply.github.com>
2023-08-26 22:21:11 +0530

    Add files via upload

--- code/bert.py
+++ code/bert.py
@@ -11,7 +11,7 @@ from transformers import BertTokenizer, BertModel, BertConfig
 from torch import cuda
 device = 'cuda' if cuda.is_available() else 'cpu'
 
-df = pd.read_csv(r'/Users/admin/Documents/Manipal/MRM/FINAL_TASK/final_task/preprocessed_train.csv')
+df = pd.read_csv(r'C:\Users\KRISH DIDWANIA\Desktop\Dataset\preprocessed_train.csv')
 
 new_df = df[['text', 'is_humor']].copy()
 
@@ -71,25 +71,38 @@ train_params = {'batch_size': TRAIN_BATCH_SIZE,
 training_loader = DataLoader(training_set, **train_params)
 
 
-class BERTClass(torch.nn.Module):
+class BERTClassWithFusion(nn.Module):
     def __init__(self):
-        super(BERTClass, self).__init__()
-        self.l1 = transformers.BertModel.from_pretrained('bert-base-uncased')
-        self.l2 = torch.nn.Dropout(0.3)
+        super(BERTClassWithFusion, self).__init__()
+        self.l1 = BertModel.from_pretrained('bert-base-uncased')
+        self.l2 = nn.Dropout(0.3)
         self.lstm = nn.LSTM(input_size=768, hidden_size=128, num_layers=1, bidirectional=True, batch_first=True)
-        self.l3 = torch.nn.Linear(256, 6)  # Update the input size to 256 due to bidirectional LSTM
-        self.l4 = torch.nn.Linear(6, 1)
-
+        self.l3_bilstm = nn.Linear(256, 6)
+        self.l3_linear = nn.Linear(768, 6)  # Linear layer for BERT output
+        self.l4 = nn.Linear(6, 1)
+        
     def forward(self, ids, mask, token_type_ids):
         output_1 = self.l1(ids, attention_mask=mask, token_type_ids=token_type_ids)
         last_hidden_state = output_1.last_hidden_state
+        
+        # Linear transformation of BERT output
+        bert_output = self.l3_linear(last_hidden_state)
+        
         output_2 = self.l2(last_hidden_state)
         lstm_output, _ = self.lstm(output_2)  # Use LSTM layer
-        output = self.l3(lstm_output)
-        output = self.l4(output)
-        return output
+        
+        # Linear transformation of BiLSTM output
+        lstm_output = self.l3_bilstm(lstm_output)
+        
+        # Concatenate BERT output and BiLSTM output
+        fused_output = torch.cat((bert_output, lstm_output), dim=2)
+        
+        fused_output = self.l4(fused_output)
+        return fused_output
 
-model = BERTClass()
+# Create an instance of the model
+model = BERTClassWithFusion()
+
 model.to(device)
 
 def loss_fn(outputs, targets):
