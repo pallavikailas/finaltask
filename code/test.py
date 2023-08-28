7224ff92380dc1a3fb0e13f3642314337bc355ed
krish0674 <118186942+krish0674@users.noreply.github.com>
2023-08-28 19:10:19 +0530

    Add files via upload

--- code/test.py
+++ code/test.py
@@ -6,10 +6,18 @@ import torch
 from tqdm import tqdm
 from torch import nn
 
-device='cpu'
+from torch import cuda
+device = 'cuda' if cuda.is_available() else 'cpu'
 
+MAX_LEN = 200
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 4
+EPOCHS = 10
+LEARNING_RATE = 1e-05
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model=BERTClassWithFusion()
-model.load_state_dict(torch.load('/kaggle/working/NLP_model.pt'))
+model.load_state_dict(torch.load('/kaggle/input/trainedmodel/NLP_model.pt'))
+model.to(device)
 
 df2 = pd.read_csv(r'/kaggle/input/nlpfinal/preprocessed_test.csv')
 
@@ -27,8 +35,9 @@ test_params = {'batch_size': VALID_BATCH_SIZE,
 test_loader = DataLoader(test_set, **test_params)
 
 def testing(model):
-    all_targets = []
-    all_outputs = []
+    model.eval()
+    fin_targets = []
+    fin_outputs = []
     with torch.no_grad():
         for _, data in enumerate(test_loader, 0):
             ids = data['ids'].to(device, dtype=torch.long)
@@ -36,16 +45,13 @@ def testing(model):
             token_type_ids = data['token_type_ids'].to(device, dtype=torch.long)
             targets = data['targets'].to(device, dtype=torch.float)  # Transfer targets to GPU
             outputs = model(ids, mask, token_type_ids)
-            all_targets.append(targets)
-            all_outputs.append(torch.sigmoid(outputs))
-            
-        all_targets = torch.cat(all_targets, dim=0).cpu().numpy()  # Transfer back to CPU for metrics
-        all_outputs = torch.cat(all_outputs, dim=0).cpu().numpy()  # Transfer back to CPU for metrics
-            
-        binary_outputs = (all_outputs >= 0.5).astype(int)
-        accuracy = metrics.accuracy_score(all_targets, binary_outputs)
-        f1_score_macro = metrics.f1_score(all_targets, binary_outputs, average='macro')
-        print(f"Accuracy Score = {accuracy}")
-        print(f"F1 Score (Macro) = {f1_score_macro}")
+            fin_targets.extend(targets.cpu().detach().numpy().tolist())
+            fin_outputs.extend(torch.sigmoid(outputs).cpu().detach().numpy().tolist())
+    return fin_outputs, fin_targets
 
-testing(model)
+out2, targ2 = testing(model)
+out2= (np.array(out2) >= 0.5).astype(int)    
+accuracy2 = metrics.accuracy_score(targ2, out2)    
+f1_score_macro2 = metrics.f1_score(targ2, out2, average='macro')
+print(f"Accuracy Score Test = {accuracy2}")
+print(f"F1 Score (Macro) Test = {f1_score_macro2}")
